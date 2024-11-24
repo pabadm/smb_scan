2d83e3710fae28fa897aa37bd60800d96c8ebbb7
pabadm <balanpasha9@gmail.com>
2024-11-24 10:57:06 -0500

    init

---
+++ ms17_10/check_ms17_10.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import argparse
+import re
+import sys
+
+from .mysmb import MYSMB
+
+
+'''
+Script for
+- check target if MS17-010 is patched or not.
+- find accessible named pipe
+'''
+
+def check_ms17_10(target_ip, port, username, password):
+    delimiter = '*' * 30
+
+    connection = MYSMB(target_ip, int(port))
+    connection.login_or_fail(username, password)
+
+    tid = connection.tree_connect_andx('\\\\' + target_ip + '\\' + 'IPC$')
+    connection.set_default_tid(tid)
+
+    result = connection.check_ms17_010()
+    
+    connection.disconnect_tree(tid)
+    connection.logoff()
+    connection.get_socket().close()
+
+    
+    return result
---
+++ smb_scan.py
@@ -0,0 +1,32 @@
+import argparse
+from ms17_10.check_ms17_10 import check_ms17_10
+
+def create_parser():
+    parser = argparse.ArgumentParser(description="Check SMB vulnerabilities.")
+    # Positional argument for IP address
+    parser.add_argument("ip", type=str, help="IP address of the target to check for SMB vulnerabilities.")
+    # Optional argument for port with a default value of 445
+    parser.add_argument("--port", type=int, default=445, help="Port to use for SMB (default is 445).")
+    parser.add_argument("--username", type=str, default="", help="Port to use for SMB (default is '').")
+    parser.add_argument("--password", type=str, default="", help="Port to use for SMB (default is '').")
+    return parser
+
+def main():
+    parser = create_parser()
+    args = parser.parse_args()
+    
+    ip = args.ip
+    port = args.port
+    username = args.username
+    password = args.password
+    delimiter = '*' * 30
+    print(f"[*] Checking SMB vulnerabilities for IP: {ip} on port {port}")
+    print(delimiter)
+    print(f"[*] Checking for the MS17-010 exploits")
+    print(f"[*] [*] EternalBlue, EternalRomance, SMBRelay (CVE-2017-0143, CVE-2017-0144, CVE-2017-0145)")
+    check_ms17_10(ip, port, username, password)
+    print(delimiter)
+    # Here you can add the logic to check SMB vulnerabilities for the provided IP and port.
+
+if __name__ == "__main__":
+    main()
